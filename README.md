af7f4746c9194a826b4c1c6d7a1dc99d22f1906f
Alec Graham <110301680+alecisalec@users.noreply.github.com>
2022-08-15 13:27:51 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# fuzzy-disco
+
+
+#how to use:
+
+#from the commandline:
+
+python3 rollingAverages.py [STOCK_TICKER] [TIME_PERIOD] [ROLLING_AVERAGE]
+
+#example:
+
+#if you want to see the rolling averages of Coca Cola 
+#over a 100 day time period with a 5 day 
+#rolling average:
+
+python3 rollingAverages.py KO 100 5
---
+++ rollingAverages.py
@@ -0,0 +1,45 @@
+import numpy as numpy
+import pandas
+import requests 
+import math
+import xlsxwriter
+import yfinance as yf
+import matplotlib.pyplot as plt
+import sys
+
+#curl -XGET 'https://tradestie.com/api/v1/apps/reddit'
+
+
+#command line arguments
+ticker = sys.argv[1]
+#print(ticker)
+dateRange = sys.argv[2]
+dateRange = int(dateRange)
+#print(dateRange)
+rollingAveRange = sys.argv[3]
+rollingAveRange = int(rollingAveRange)
+#print(rollingAveRange)
+
+
+# ##Creating Moving averages Data with coca-cola stocks
+Stock = yf.download(ticker)
+stock=Stock.Close.to_frame()
+# print(stock)
+stock = stock.tail(dateRange)
+
+### calculate moving averages calulated in 10 day windows
+stockRoll=stock.rolling(window=rollingAveRange)
+stockRoll=stockRoll.mean()
+## show only last 30 days not entire historical data
+stockRoll = stockRoll.tail(dateRange)
+
+## configure the map output
+plt.figure(figsize=(10,5))
+plt.plot(stock, 'k-', label="original closing prices")
+plt.plot(stockRoll, 'r-', label="rolling average")
+plt.ylabel('Price')
+plt.xlabel('Date')
+plt.grid(linestyle=':')
+plt.legend(loc='upper left')
+plt.title(f"{ticker} over {dateRange} days with a {rollingAveRange} day rolling average period")
+plt.show()
